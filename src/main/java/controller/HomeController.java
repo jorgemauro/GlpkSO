1c1173084be44dd138e9227a81f398084f785dd9
jorge <jorge mauro goncalves>
2017-11-14 16:09:43 -0200

    correção de posicionamento de packge

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -3,6 +3,7 @@ package controller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @EnableAutoConfiguration
 public class HomeController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String execute() {
+    public String execute(Model model) {
         return ("index");
     }
     public static void main(String[] args) throws Exception {
